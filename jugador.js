727cdfa3927dcc4ac8ff8fff72ec49dec9315c07
CicloSuperior <CicloSuperior>
2016-11-24 16:27:31 +0100

    jugador mod

--- jugador.js
+++ jugador.js
@@ -1,72 +1,94 @@
 'use strict';
-var Partida=require('./partida.js');
-var Elemento=require('./elemento.js');
+var Partida = require('./partida.js');
+var Elemento = require('./elemento.js');
 
-class Jugador{
+class Jugador {
 
-	constructor(nombre,id){
-		this._nombre=nombre;
-		this._id=id;
-		this._tanques= new Map();
+	constructor(nombre, id) {
+		this._nombre = nombre;
+		this._id = id;
+		this._tanques = new Map();
 	}
 
-	set nombre(nombre){
-		this._nombre=nombre;
+	set nombre(nombre) {
+		this._nombre = nombre;
 	}
 
-	get nombre(){
+	get nombre() {
 		return this._nombre;
 	}
 
-	set id(id){
-		this._id=id;
+	set id(id) {
+		this._id = id;
 	}
 
-	get id(){
+	get id() {
 		return this._id;
 	}
 
-	get tanques(){
+	get tanques() {
 		// return Array.from(this._tanques);
 		return this._tanques;
 	}
 
-	crearPartida(inf){
-		
-		var partida = new Partida(inf.nombre,inf.tablero.c,inf.tablero.f);
-		// console.log(partida);
-		partida.addJugador(this);
-		return partida;
+	crearPartida(inf,cb) {
+		var jugador=this;
+		var partida = new Partida(inf.nombre, inf.tablero.c, inf.tablero.f);
+
+		partida.comprobarPartida(inf.nombre,function(data){
+			if(data>0){
+				//callback
+				cb('ya existe');
+			}else{
+				partida.addJugador(jugador);
+				cb(partida);
+			}
+			
+		})
+				// partida.comprobarPartida(inf.nombre, function(data,partida,jugador) {
+		// 	if (data == 0) {
+		// 		partida.addJugador(jugador);
+		// 		cb(partida);
+		// 	} else {
+		// 		cb({
+		// 			err: "la partida ya existe"
+		// 		});
+		// 	}
+		// });
+
+
+
 	}
 
-	unirse(){
+
+	unirse() {
 
 	}
 
-	salir(){
+	salir() {
 
 	}
 
-	crearTanque(nombre){
-		var nuevoTanque= new Elemento.tanque(nombre);
-		this._tanques.set(nombre,nuevoTanque);
+	crearTanque(nombre) {
+		var nuevoTanque = new Elemento.tanque(nombre);
+		this._tanques.set(nombre, nuevoTanque);
 	}
 
-	opciones(){
+	opciones() {
 
 	}
 
-	buscarTanque(nombre){
+	buscarTanque(nombre) {
 		return this._tanques.get(nombre);
 	}
 
-	borrarTanque(nombre){
+	borrarTanque(nombre) {
 		this._tanques.delete(nombre);
 	}
 
-	cargarJugador(){
-		
+	cargarJugador() {
+
 	}
 }
 
-module.exports=Jugador;
+module.exports = Jugador;
